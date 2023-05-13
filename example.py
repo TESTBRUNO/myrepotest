156c6229485872d06a61e7a031db9e91e0a55f86
Nikita <brun58@ya.ru>
2023-05-13 13:06:53 +0300

    Коммит в новой ветке

--- example.py
+++ example.py
@@ -6,23 +6,22 @@ git commit -m "<Ваше сообщение, любое>"
 
 git status - узнать статус гит на текущий момент
 
+git pull - синхрон
+
+git checkout -b testBranch
 
 
 cmdkey /delete:LegacyGeneric:target=git:https://github.com
 
-<<<<<<< HEAD
-Изменияю код. Всем привет!
-=======
 dadadadadadad
 
->>>>>>> 5eb0ad1991c38a03bd5971c83bad3a47480d9e52
 
 rfr nj nfr
 futyuftyufty
 
 fsdfrsdfsd
 igjhjhgjkhgjk
-guasdasdasd
+gu
 
-asdasdasd
+dasdasd
 sdfsdfsdf
