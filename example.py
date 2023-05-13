0ac99d4fc6ca73296ecc348aa46f036850918242
brun58 <brun58@ya.ru>
2023-05-13 12:00:40 +0300

    второй коммит

--- example.py
+++ example.py
@@ -3,3 +3,13 @@ git init - Создание репозитория
 git add <путь к файлу> - Добавление файла в область видимости гит
 
 git commit -m "<Ваше сообщение, любое>"
+
+git status - узнать статус гит на текущий момент
+
+
+
+cmdkey /delete:LegacyGeneric:target=git:https://github.com
+
+
+
+вфывфывфывфывфы
